f9e282d8f99b5bbcfc8f3b9446ab068c545dd885
litonp <litondas7121@gmail.com>
2023-09-15 18:36:51 +0600

    ball

---
+++ variable.js
@@ -0,0 +1,37 @@
+// number type variable
+var sanglassPrice = 120;
+
+// string type variable
+var specialOneName = 'Ariana Grande';
+
+g//boolean type variable 
+var isSingle = true;
+
+var price1 = 10;
+var price2 = 20;
+var price3 = 20;
+var totalPrice = 'price1 + price2 + price3';
+console.log(totalPrice);
+
+// two type of number:
+//1.Integer: 120,   //2.Float, decimal: 3.65,
+
+// variable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or speace
+ * 3. no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+ */ 
+
+// Math Operation: +, -, *, /,
+// Shorthand: +=, -=, ++, --
+
+// convert: parsenInt, parseFloat
+
+// toFixed
+
+// remainder : vagses: %
