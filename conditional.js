2eebd79f25d0cabd6c36aa12bc13f178b193b44d
litonp <litondas7121@gmail.com>
2023-09-16 20:06:09 +0600

    potato

---
+++ conditional.js
@@ -0,0 +1,32 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if iphone price is less than my budget. I will buy the iphone
+/**
+ * if iphone price is less then my budget 
+ *   i will buy the iphone 
+ * 
+ * if (conditional) {
+//  *       will execute if the condition is true
+* }
+*/
+
+if (iphonePrice < myBudget) {
+    console.log('i phone kinbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if (chickenPrice <= myMoney) {
+    console.log('khabo');
+}
+if (chickenPrice > myMoney) {
+    console.log('khabo na');
+}
+
+// 
+if (chickenPrice < myMoney) {
+    console.log('i will eat chicken');
+}
+else {
+    console.log('i will eat potato');
+}
