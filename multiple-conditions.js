e07c35aa78857c55b537d12599fbb7b176ec3aa6
litonp <litondas7121@gmail.com>
2023-09-16 22:07:34 +0600

    cars

---
+++ multiple-conditions.js
@@ -0,0 +1,36 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if (isGraduated == true) {
+//     console.log('biye kore peli');
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+
+// if (isGraduated == true && salary > 50000) {
+//     console.log('biye kore peli');
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+
+// if (isGraduated == true && salary > 50000 && cars >= 1) {
+//     console.log('biye kore peli');
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+
+
+var isGraduated = false;
+var salary = 5000;
+var cars = 1;
+
+if (isGraduated === true || salary > 50000 || cars >= 1) {
+    console.log('Eso prem kori');
+}
+else {
+    console.log('tomar friend er num deo');
+}
