3e7b9ed479f1a484be377fcd40b3d3a189db438a
litonp <litondas7121@gmail.com>
2023-09-16 15:43:18 +0600

    array

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFree = [5000, 2000, 4000, 250];
+var nayikas = ['mahi', 'opu', 'shsbnur', 'popi'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(friendsAge.length);
