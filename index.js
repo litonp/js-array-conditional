8343bbb6fafd19bc498da9a95288f082500ab037
litonp <litondas7121@gmail.com>
2023-09-16 16:04:02 +0600

    index

---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// get element by index 
+var element = numbers[5]
+console.log(element);
+
+// set element value by index
+numbers[1] = 77;
+numbers[3] = 64;
+console.log(numbers);
+
+// find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
