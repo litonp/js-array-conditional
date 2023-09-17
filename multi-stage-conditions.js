3cef9656814ca854914a7dd1f1062f5fc1213dc4
litonp <litondas7121@gmail.com>
2023-09-17 19:01:29 +0600

    khabo

---
+++ multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 46;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if (danishPrice < money) {
+    console.log('danish khao');
+}
+else if (butterBread < money) {
+    console.log('butter bon khabo');
+}
+else if (toastBiscuit < money) {
+    console.log('toast biscuit khabo');
+}
+else {
+    console.log('khali cha e shasther jonno valo');
+}
