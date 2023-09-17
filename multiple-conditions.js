1addd474eb34615e8dc5152eb13c9e854d861b02
litonp <litondas7121@gmail.com>
2023-09-17 19:12:28 +0600

    last

--- multiple-conditions.js
+++ multiple-conditions.js
@@ -1,28 +1,26 @@
-// var isGraduated = true;
-// var salary = 75000;
-// var cars = 1;
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
 
-// if (isGraduated == true) {
-//     console.log('biye kore peli');
-// }
-// else {
-//     console.log('tor kopale biya nai')
-// }
+if (isGraduated == true) {
+    console.log('biye kore peli');
+}
+else {
+    console.log('tor kopale biya nai')
+}
 
-// if (isGraduated == true && salary > 50000) {
-//     console.log('biye kore peli');
-// }
-// else {
-//     console.log('tor kopale biya nai')
-// }
-
-// if (isGraduated == true && salary > 50000 && cars >= 1) {
-//     console.log('biye kore peli');
-// }
-// else {
-//     console.log('tor kopale biya nai')
-// }
-
+if (isGraduated == true && salary > 50000) {
+    console.log('biye kore peli');
+}
+else {
+    console.log('tor kopale biya nai')
+}
+if (isGraduated == true && salary > 50000 && cars >= 1) {
+    console.log('biye kore peli');
+}
+else {
+    console.log('tor kopale biya nai')
+}
 
 var isGraduated = false;
 var salary = 5000;
